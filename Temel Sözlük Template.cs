0fcb4047d6de6eda872d5cc369ffb2f9ea80b7ce
kaanbaylar <71941031+kaanbaylar@users.noreply.github.com>
2023-04-11 20:05:07 +0300

    Add files via upload
    The dictionary search was written in the "Temel Sözlük Template.cs" file. The dictionary database is "SozlukVeri.txt".

---
+++ Temel Sözlük Template.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Dictionary<string, string> sozluk = new Dictionary<string, string>();
+
+        // SozlukVeri.txt dosyasýný açýp okuma iþlemi yapýlýr.
+        using (StreamReader sr = new StreamReader("SozlukVeri.txt"))
+        {
+            string line;
+            string anahtar = "";
+            string aciklama = "";
+            while ((line = sr.ReadLine()) != null)
+            {
+                if (line.StartsWith("-s/"))
+                {
+                    anahtar = line.Substring(3);
+                }
+                else if (line.StartsWith("-a/"))
+                {
+                    aciklama = line.Substring(3);
+                    sozluk.Add(anahtar, aciklama);
+                }
+            }
+        }
+
+        while (true)
+        {
+            Console.WriteLine("Aranacak kelimeyi girin (çýkmak için é):");
+            string anahtarKelime = Console.ReadLine();
+
+            if (anahtarKelime.ToLower() == "é")
+            {
+                break;
+            }
+
+            if (sozluk.ContainsKey(anahtarKelime))
+            {
+                Console.WriteLine($"{anahtarKelime}: {sozluk[anahtarKelime]}");
+            }
+            else
+            {
+                Console.WriteLine($"{anahtarKelime} sözlükte bulunamadý.");
+            }
+        }
+    }
+}
